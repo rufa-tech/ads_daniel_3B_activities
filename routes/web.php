1ce18684f849c9bb4942a439825e21ae25d210ef
unknown <rufamaedaniel89@gmail.com>
2024-09-05 00:19:47 -0700

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/students/create', function(){
+$student  = new student();
+$student->first_name = 'Rufa Mae';
+$student->last_name = 'Daniel';
+$student->email = 'rufamaedaniel89gmail.com';
+$student->save();
+return 'Student Created!';
+});	
+
+Route::get('/students', function (){
+	$students = student::all();
+	return $students;
+});
+
+Route::get('/students/update', function (){
+	$student = student::where('email', 'rufamaedaniel89@gmail.com')->first();
+	$student->email = 'rufamaedaniel89@gmail.com';
+	$student->save();
+	return 'Student Updated!';
+});
+
+Route::get('/students/delete', function (){
+	$student = Student::where('email', 'rufamaedaniel89')->first();
+	$student->delete();
+	return 'Student Deleted';
+
+
+});
+
+Route::get('/couirses/create', function (){
+	$course = new Course();
+	$course->course_name = 'Introduction od Databases';
+	$course->save();
+	return 'Course Created!';
 });
